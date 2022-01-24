be9911fb46ea165007deaa636f3dbf861aa4b98c
hooman <hoomanm87@gmail.com>
2022-01-24 22:30:49 +0330

    feat: Create new components & install new packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import SideBar from "./componetns/TheSideBar"
+import MainContent from "./componetns/MainContent"
+import { useEffect, useState } from "react"
+import axios from "axios"
 function App() {
+  const [location, setLocation] = useState({})
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+
+    // axios.get("https://api.weatherapi.com/v1/current.json?key=1db779ded4e849c989a175725221301&q=tehran&aqi=yes")
+    //   .then(res => {
+    //     setLocation(res.data.location)
+    //     setWeather(res.data.current)
+    //   })
+    //   .catch(err => console.log(err))
+  }, [])
   return (
     <>
-     
+      <div className="app">
+        <SideBar />
+        <MainContent />
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/componetns/TheSideBar.jsx
@@ -0,0 +1,31 @@
+function SideBar(props) {
+  // const location = props.location
+  // const weather = props.weather
+  const datetime = new Date()
+  const time = datetime.toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' })
+  const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "Saturday"]
+  setTimeout(() => { }, 1000)
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <form method="post" className="sidebar-form">
+          <i class="fas fa-search"></i>
+          <input type="text" placeholder="Search for places..." className="sidebar-form__input" />
+        </form>
+      </div>
+      <div className="sidebar-body">
+        <div style={{ display: 'grid', gridTemplateColumns: 'auto auto', gridTemplateRows: '1fr', alignItems: 'center' }}>
+          <img src="https://cdn.weatherapi.com/weather/64x64/day/112.png" className="sidebar-body__icon" />
+          <p className="sidebar-body__title"><span>12</span><sup>°C</sup></p>
+        </div>
+        <p className="sidebar-body__text">Sunny</p>
+        <p className="sidebar-body__text">{days[datetime.getDay()]}, <span className="text-secondary">{time}</span></p>
+      </div>
+      <div className="sidebar-footer">
+        <p>tehran, tehran, iran</p>
+      </div>
+    </aside>
+  );
+}
+
+export default SideBar;
